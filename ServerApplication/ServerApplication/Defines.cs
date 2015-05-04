83a855ccc68edb410ebf5cbc1cb419399742c78d
ReutovIgor <ireutov_777@rambler.ru>
2015-05-05 02:13:29 +0300

    First defined structure is added.

---
+++ ServerApplication/ServerApplication/Defines.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defines
+{
+    //Defines.ConsultationData data = new Defines.ConsultationData();
+    public struct ConsultationData
+    {
+        DateTime date_time;
+        String department;
+        int patientId;
+        int doctorId;
+
+        public ConsultationData(DateTime date_time, String department, int patientId, int doctorId)
+        {
+            this.date_time = date_time;
+            this.department = department;
+            this.patientId = patientId;
+            this.doctorId = doctorId;
+        }
+    }
+}
