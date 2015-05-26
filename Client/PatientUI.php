1be7e0e6b4062226f0c4911596fef826c54abecb
unknown <bolotov.m@ws-2k-220-a3.ccam.cmc.unn.net>
2015-05-26 16:32:42 +0300

    обновлена клиентская часть и добавлена нумерация в документы

--- Client/PatientUI.php
+++ Client/PatientUI.php
@@ -3,17 +3,37 @@
   <title>Пользователь</title>
  </head>
  <style type="text/css">
-  .header
+  .header1
   {
-  min-width: 1280px;
-  max-width: 1600px;
-  margin: 0 auto;
-  padding: 0;
+  width: 226px;
   position:absolute;
   top:0px;
   left:0px;
   background:#ccc;
   border: solid 1px black; 
+  text-align: center;
+}
+
+.header2
+  {
+  width: 200px;
+  position:absolute;
+  top:0px;
+  left:228px;
+  background:#ccc;
+  border: solid 1px black; 
+  text-align: center;
+}
+
+.header3
+  {
+  width: 200px;
+  position:absolute;
+  top:0px;
+  left:430px;
+  background:#ccc;
+  border: solid 1px black; 
+  text-align: center;
 }
  </style>
  <style>
@@ -26,56 +46,103 @@
  <style>
  #form-container 
    {
-    display: none
+    display: none;
    }
 
-   #form-container1 
+   #form-container2 
    {
-    display: none
+    display: none;
    }
    </style>
 
    <style type="text/css">
    .block1 { 
-    width: 250px; 
+    width: 201px; 
     background: #ccc;
     padding: 5px;
     padding-right: 20px; 
     border: solid 1px black; 
     position: absolute;
-    top: 200px;
-    left: 380px;
+    top: 35px;
+    left: 0px;
+   }
+
+   .block2 { 
+    width: 175px; 
+    background: #ccc;
+    padding: 5px;
+    padding-right: 20px; 
+    border: solid 1px black; 
+    position: absolute;
+    top: 35px;
+    left: 228px;
+    text-align: center;
    }
    </style>
 
-   <div id="header" class="header">
- <form style="header"><button href="#form-container" rel="get_consultatiom_win" id="button_get_consultation">Записаться на приём</button> <button href="#form-container1" rel="get_history_win" id="button_get_history">История</button>  <button style="header_button" name="button_get_online_consultation">Онлайн-консультация</button></form>
+   <div id="header" class="header1">
+ <form><a href="#form-container" rel="get_consultatiom_win" id="button_get_consultation">Записаться на приём</a> </form>
  </div>
+
+ <div id="header" class="header2">
+ <form><a href="#form-container" rel="get_history_win" id="button_get_history">История</a> </form>
+ </div>
+
+ <div id="header" class="header3">
+ <form><a href="#form-container" rel="get_history_win" id="button_get_history">Онлайн-консультация</a> </form>
+ </div>
+
+ <div id="form-container2" class="block2">
+<form id="get_history_win" name="Get_history_win">
+<p>Здесь будет отображаться история ваших действий</p>
+</form>
+</div>
 
 <div id="form-container" class="block1">
 <form id="get_consultation_win" name="Get_consultation_win">
-    <p>Фамилия:<input type="text" name="surname" size="30"></p>
-    <p>Имя:<input type="text" name="name" size="30"></p>
-    <p>Пароль:<input type="password" name="password" size="30"></p>
-    <p>Повторите:<input type="password" name="repeat_password" size="30"></p>
+    <p>
+    Выберите филиал: 
+    <select>
+      <option>Нижний Новгород</option>
+      <option>Дзержинск</option>
+      <option>Муром</option>
+      <option>Правдинск</option>
+    </select>
+    </p>
+
+    <p>
+    Укажите направление:
+    <select>
+      <option> </option>
+      <option>Кардиология</option>
+      <option>Диетология</option>
+      <option>Ортопедия</option>
+    </select>
+    </p>
+
+    <p>
+    Уточните специалиста:
+    <select>
+     <option> </option>
+     <option>Нижний Новгород</option>
+    </select>
+    </p>
+
+    <p>
+    Выберите время:
+    <select>
+    <option> </option>
+    <option>Нижний Новгород</option>
+    </select>
+    </p>
+
+    
     <button name="apply">Подтвердить</button>
  </form>
  </div>
 
- <div id="form-container1" class="block1">
-<form id="get_history_win" name="Get_history_win">
-    <p>Фамилия:<input type="text" name="surname" size="30"></p>
-    <p>Имя:<input type="text" name="name" size="30"></p>
-    <p>Пароль:<input type="password" name="password" size="30"></p>
-    <p>Повторите:<input type="password" name="repeat_password" size="30"></p>
-    <button name="apply">Подтвердить</button>
- </form>
- </div>
 
- <body>
- <?php
  
- ?>
  <script>
 document.getElementById('button_get_consultation').onclick=function()
 {
@@ -84,8 +151,8 @@ document.getElementById('button_get_consultation').onclick=function()
 
 document.getElementById('button_get_history').onclick=function()
 {
-  document.getElementById('form-container1').style.display='block';
+  document.getElementById('form-container2').style.display='block';
 }
 </script>
- </body>
+
 </html>
